5a3ba0d8a827ddb19ea192d85a2e1e4302793ebc
galatea-aws <aws@galatea-associates.com>
2014-01-14 15:36:24 -0500

    1

--- run.sh
+++ run.sh
@@ -106,5 +106,4 @@ function help() {
 
 # Run the target passed as the first arg on the command line
 # If no first arg, run server
-if [ $# -gt 1 ]; then help; exit; fi
-if [ $# = 1 ]; then $1; else server; fi
+if [ $# -gt 1 ]; then $1; else server; fi
