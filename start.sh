5c27759e3fd8e81254fb45a1d6b6a7e6e265e709
galatea-aws <aws@galatea-associates.com>
2014-01-15 13:26:46 -0500

    1

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-ssh user@server bash < /path/to/local/script.sh
+echo 'start voltdb on 172.31.22.149'
+ssh voltdb@172.31.22.149 bash < /path/to/local/script.sh
+echo 'start voltdb on 172.31.22.150'
+ssh voltdb@172.31.22.150 bash < /path/to/local/script.sh
