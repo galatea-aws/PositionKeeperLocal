a61b5b095d6f60e352dced8cf5365d76dbb441c0
galatea-aws <aws@galatea-associates.com>
2014-01-15 16:48:23 -0500

    add call to shell

--- helloworld.py
+++ helloworld.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python
 import paramiko
+import time
+from subprocess import call
 from pyjavaproperties import Properties
-hostname = '172.31.22.149' 
-port = 22
-username = 'voltdb' 
-password = 'voltdb'
-print ('helloworld')
 if __name__ == "__main__":  
     paramiko.util.log_to_file('paramiko.log')
     p = Properties()
     p.load(open('serverconfig.properties'))
-    hostname = p['servers']
     s = paramiko.SSHClient()
     s.load_system_host_keys()
     s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    s.connect(hostname, port, username, password) 
-    s.exec_command('cd /home/voltdb/voltdb-3.5.0.1/examples/Positionkeeper; ./server.sh') 
+	hostlist = p['servers'].split[',']
+	port = 22
+	username = 'voltdb' 
+	password = 'voltdb'
+	for hostname in hostlist
+		print(hostname)
+		s.connect(hostname, port, username, password) 
+        s.exec_command('cd /home/voltdb/voltdb-3.5.0.1/examples/Positionkeeper; ./server.sh')
+	print('waiting for server start')
+	time.sleep(10)
+	subprocess.call(['/home/voltdb/voltdb-3.5.0.1/examples/Positionkeeper/tradesimulator.sh'])
+	subprocess.call(['/home/voltdb/voltdb-3.5.0.1/examples/Positionkeeper/querytester.sh'])
+	
     s.close()
