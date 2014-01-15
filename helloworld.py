39a0c22981739f66dc8a6f6c263543144da1a3b0
galatea-aws <aws@galatea-associates.com>
2014-01-15 21:18:14 +0000

    add pyjavaproperties

--- helloworld.py
+++ helloworld.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 import paramiko
+from pyjavaproperties import Properties
 hostname = '172.31.22.149' 
 port = 22
 username = 'voltdb' 
 password = 'voltdb'
 print ('helloworld')
-if __name__ == "__main__": 
+if __name__ == "__main__":  
     paramiko.util.log_to_file('paramiko.log')
+    p = Properties()
+    p.load(open('serverconfig.properties'))
+    hostname = p['servers']
     s = paramiko.SSHClient()
     s.load_system_host_keys()
     s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     s.connect(hostname, port, username, password) 
-    stdin, stdout, stderr = s.exec_command('getconf PATH') 
+    s.exec_command('cd /home/voltdb/voltdb-3.5.0.1/exmaples/Positionkeeper; ./server.sh') 
     print stdout.read()
     s.close()
