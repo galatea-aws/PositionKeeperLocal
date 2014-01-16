45b5346baa0186a41a075f8864a53393417eb299
galatea-aws <aws@galatea-associates.com>
2014-01-16 08:58:08 -0500

    test

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-. serverconfig.properties
-SERVER_SH = "$VOLTDB_BASE/examples/PositionKeeper/server.sh"
-echo 'start voltdb on 172.31.22.149'
-ssh voltdb@172.31.22.149 bash < $SERVER_SH
-echo 'start voltdb on 172.31.22.150'
-ssh voltdb@172.31.22.150 bash < $SERVER_SH
